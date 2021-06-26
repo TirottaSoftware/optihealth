a98c9965bd687e7c0a69d364066d6ce48c0de2c0
Cristian <i449915@student.fontys.nl>
2021-06-26 21:10:31 +0300

    Added products page

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ var sidebar = document.querySelector(".sidebar");
 burger.addEventListener("click", ()=>{
     sidebar.classList.toggle("hidden");
 })
+
+var logo = document.querySelector(".logo");
+
+logo.addEventListener("click", () => {
+    window.location.href = './index.html';
+})
